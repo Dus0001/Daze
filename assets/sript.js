96628cbe47c7e11f04b31e6ce920495ec4717709
Daven <davensteen@gmail.com>
2022-09-17 17:13:32 -0500

    add time to id

--- assets/sript.js
+++ assets/sript.js
@@ -1,6 +1,58 @@
 var currentTimeEl = document.querySelector(".lead");
+let eventHour = 0;
 
 setInterval(function() {
 currentTimeEl.textContent = moment().format("LLLL");
+
 },
 1000);
+
+ var nineAm = moment().hour("9");
+ var currentHour = moment().hour();
+
+   
+
+var auditHours = function(eventHour) {
+    $("li").each(function(){
+        
+        var getEventHour = $(this).attr("id");
+        console.log(getEventHour)
+
+        //remove any old lasses from element
+    $("textarea").removeClass("present");
+  
+    // apply new class if task is near/over due date
+    if (currentHour > getEventHour) {
+        $("textarea").addClass("past");
+        console.log("past")
+
+      }
+      else if (currentHour < (getEventHour)) {
+        $("textarea").addClass("future");
+        console.log("future")
+      }  
+      else {
+        $("textarea").addClass("present");
+        console.log("present")
+      }       
+
+    })
+    
+  eventHour++
+  };
+
+  // event  listener for save button
+
+  $(".saveBtn").on("click",function() {
+    //send event and time to local storage
+    var eventName = $(this).siblings("textarea").val;
+    console.log(eventName);
+    var blockTime = $(this).parent().attr("id");
+    console.log(blockTime);
+
+   //save in localStorage
+   localStorage.setItem(eventName, blockTime);
+   console.log("saved")
+  });
+
+  auditHours();
