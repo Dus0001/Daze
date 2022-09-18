c97a59902279a695c4b84707cd7833f7b5e8c1a0
Daven <davensteen@gmail.com>
2022-09-17 19:51:59 -0500

    removed console.log

--- assets/sript.js
+++ assets/sript.js
@@ -7,7 +7,6 @@ currentTimeEl.textContent = moment().format("LLLL");
 },
 1000);
 
- var nineAm = moment().hour("9");
  var currentHour = moment().hour();
 
    
@@ -17,7 +16,7 @@ var auditHours = function() {
     $("li").each(function(eventHour){
         
         var getEventHour = $(this).attr("id");
-        console.log(getEventHour)
+        
 
         //remove any old classes from element
     $("textarea").removeClass("present");
@@ -30,11 +29,10 @@ var auditHours = function() {
       }
       else if (currentHour < (getEventHour)) {
         $(this).children("textarea").addClass("future");
-        console.log("future")
       }  
       else {
         $(this).children("textarea").addClass("present");
-        console.log("present")
+       
       }  
       eventHour++     
 
@@ -44,17 +42,18 @@ var auditHours = function() {
 
   // event  listener for save button
 
-  $(".saveBtn").on("click",function() {
+  $(".saveBtn").on("click",function(event) {
     //send event and time to local storage
     var eventName = $(this).siblings("textarea").val();
-    console.log(eventName);
+
     var blockTime = $(this).parent().attr("id");
-    console.log(blockTime);
 
    //save in localStorage
    localStorage.setItem(JSON.stringify(eventName), blockTime);
-   console.log("saved")
   });
+  
+  //$(".saveBtn").forEach(localStorage.getItem(JSON.parse(eventName)));
+  
   
   setInterval(function() {
     //$(".time-block").each(function() {
